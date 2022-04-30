0b15009cb95b152dde900fdb5a33adb522e15619
denvey <dengwei87>
2022-04-30 17:48:58 +0800

    feat: add readme

--- README.md
+++ README.md
@@ -4,3 +4,20 @@ http://w.qqshsh.com/web/index.php?c=site&a=entry&do=index&m=greatriver_lottery_o
 
 账号：isuperstar8
 密码：
+
+### 小程序取名
+
+潮玩
+手办
+盲盒
+动漫
+泡泡
+玩具
+
+玩具
+大娃
+
+抽盒机
+
+潮玩火星丨动漫手办玩具泡泡卡通
+盲盒抽盒机，潮玩动漫手办，新奇好货
--- siteinfo.js
+++ siteinfo.js
@@ -1,8 +1,16 @@
 module.exports = {
-    uniacid: "3",
-    acid: "3",
-    multiid: "0",
-    version: "2.0.0",
-    siteroot: "https://w.qqshsh.com//app/index.php",
-    design_method: "3"
+  uniacid: "3",
+  acid: "3",
+  multiid: "0",
+  version: "2.0.0",
+  siteroot: "https://w.qqshsh.com/app/index.php",
+  design_method: "3",
+
+  // 测试
+  //   uniacid: "30",
+  //   acid: "30",
+  //   multiid: "0",
+  //   version: "2.0.0",
+  //   siteroot: "https://we7.hnhxck.com/app/index.php",
+  //   design_method: "3",
 };
